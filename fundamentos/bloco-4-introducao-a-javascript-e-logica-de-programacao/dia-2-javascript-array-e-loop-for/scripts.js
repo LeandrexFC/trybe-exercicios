6126827e9f8d36b5ed1e5ee7c079eaeb2ef61486
LeandreXFC <leandro.sousa23tec@gmail.com>
2022-08-19 19:04:16 -0300

    Exercício 5 concluído

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
@@ -1,17 +1,10 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let soma = 0
+let maiorNum = 0
 
-for (let index = 0; index < numbers.length; index +=1) {
-    soma += numbers[index];
+for(let i = 0; i < numbers.length; i += 1 ) {
+     if (numbers[i] > maiorNum ) {
+        maiorNum = numbers[i]
+     }                              
 }
 
-let med = soma/numbers.length
-console.log(med);
-
-if (med >20) {
-    console.log("valor maior que 20")
-}
-
-else {
-    console.log("valor menor ou igual a 20")
-}
+console.log(maiorNum)
