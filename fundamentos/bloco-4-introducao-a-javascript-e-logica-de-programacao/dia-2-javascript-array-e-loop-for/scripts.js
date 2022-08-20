0db5d41189f04d03fc35aefe7833a2d384190cbe
LeandreXFC <leandro.sousa23tec@gmail.com>
2022-08-19 21:45:34 -0300

    Exercício 7 concluído

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
@@ -1,15 +1,23 @@
+// 6. Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let impar = []
 
-
-for(let i = 0; i < numbers.length; i += 1 ) {
-     if (numbers[i] % 2 !==0 ) {
-
+for (let i = 0; i < numbers.length; i += 1) {
+    if (numbers[i] % 2 !== 0) {
         impar.push(numbers[i])
-        console.log ("Número ímpar: ", numbers[i])
-
-        
-     }
-                             
+    }
 }
-console.log("assadsa", impar);
+//console.log(impar);
+
+// 7. Utilizando for, descubra qual o menor valor contido no array e imprima-o
+
+let menorNum = numbers[0]
+
+for (let i = 0; i < numbers.length; i += 1 ) {
+    if (numbers[i] < menorNum) {
+        menorNum = numbers[i]
+    }
+}
+
+console.log('joao:',menorNum);
