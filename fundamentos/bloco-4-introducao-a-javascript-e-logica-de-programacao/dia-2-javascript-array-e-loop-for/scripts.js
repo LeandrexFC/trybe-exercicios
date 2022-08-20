2f2bd61c3a277ce444f86c1faf2bb47b2696aa4f
LeandreXFC <leandro.sousa23tec@gmail.com>
2022-08-19 22:00:29 -0300

    Exercício 8 concluído

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
@@ -20,4 +20,14 @@ for (let i = 0; i < numbers.length; i += 1 ) {
     }
 }
 
-console.log('joao:',menorNum);
+// console.log('joao:',menorNum);
+
+// 8. Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;
+
+let num = []
+
+for (let i = 0; i <= 100; i += 1) {
+    num.push(i)
+}
+
+console.log(num);
