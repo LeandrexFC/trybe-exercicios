f1d32dbed8aa48a8d8b8218771f232327abff16a
LeandreXFC <leandro.sousa23tec@gmail.com>
2022-08-23 21:24:42 -0300

    Exercício 5 concluído

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
@@ -1,4 +1,4 @@
-//1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Valor esperado no console: Bem-vinda, Margarida
+//1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. 
 
 let info = {
     personagem: 'Margarida',
@@ -7,19 +7,38 @@ let info = {
     recorrente: 'Sim'
   };
 
-  console.log("Bem vinda " + info.personagem);
+  //console.log("Bem vinda " + info.personagem);
 
-// 🚀 2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. Valor esperado no console: 
+// 🚀 2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. 
 
-console.log(info.recorrente)
+  //console.log(info.recorrente)
 
-//3 - Faça um for/in que mostre todas as chaves do objeto. Valor esperado no console:
+//3 - Faça um for/in que mostre todas as chaves do objeto.
 
 for (let key in info) {
     //console.log([key])
 }
 
-//4 - Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto. Valor esperado no console:
+//4 - Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto. console:
 for (let key in info) {
-    console.log(info[key])
+    //console.log(info[key])
+}
+
+// 5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves.
+let info2 = {
+    personagem: 'Tio patinhas',
+    origem: 'Christmas on Bear Mountain, Dells Four Color Comics #178', 
+    nota: 'O último MacPatinhas',
+    recorrente:'Sim'
+};
+
+
+for(let key in info2){
+
+    if(info2[key] === info[key]){
+        console.log("Ambos recorrentes")
+    }else {
+        console.log(info[key] + ' e ' + info2[key]);
+}
+
 }
