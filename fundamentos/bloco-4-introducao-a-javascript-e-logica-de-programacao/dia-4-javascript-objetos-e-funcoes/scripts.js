393a82d71988efaf8479b41c611b8649b1121c43
LeandreXFC <leandro.sousa23tec@gmail.com>
2022-08-23 21:30:52 -0300

    Exercício 6 concluído

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
@@ -36,9 +36,26 @@ let info2 = {
 for(let key in info2){
 
     if(info2[key] === info[key]){
-        console.log("Ambos recorrentes")
+        //console.log("Ambos recorrentes")
     }else {
-        console.log(info[key] + ' e ' + info2[key]);
+        //console.log(info[key] + ' e ' + info2[key]);
 }
 
 }
+
+//🚀 6 - Acesse as chaves nome, sobrenome e titulo, que está dentro da chave livrosFavoritos, e faça um console.log no seguinte formato: "O livro favorito de Julia Pessoa se chama 'O Pior Dia de Todos'".
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
+
+console.log ("O livro favorito de " + leitor.nome + ' ' + "se chama " + leitor.livrosFavoritos[0].titulo + ' ' );
