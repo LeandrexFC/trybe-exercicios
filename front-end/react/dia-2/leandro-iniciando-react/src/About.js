726b5a18843f06e23909e31a5ead6fd83fec4f65
LeandreXFC <leandro.sousa23tec@gmail.com>
2022-11-19 11:53:22 -0300

    Atualiza rep

---
+++ front-end/react/dia-2/leandro-iniciando-react/src/About.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+class AboutMe extends Component {
+    render() {
+        return(
+            <>
+            <h1>
+                Leandro Sousa
+            </h1>
+            <p> Sou estudante de Desenvolvimento Web  </p>
+            <h2>
+                Minhas Habilidades
+            </h2>
+            <ul>
+                <li>Foco</li>
+                <li>Determinação</li>
+                <li>Beleza</li>
+            </ul>
+            </>
+        )
+    }
+}
+
+
+export default AboutMe;
